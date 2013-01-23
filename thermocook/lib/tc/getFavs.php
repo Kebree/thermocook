d3aa6f4f4903b7389447cc692f8724052a303892
gintrand.jb@gmail.com <gintrand.jb@gmail.com@89966a3e-d1be-d117-780a-8d1c186c44af>
2013-01-23 15:13:05 +0000

    Favourite correction

--- thermocook/lib/tc/getFavs.php
+++ thermocook/lib/tc/getFavs.php
@@ -5,7 +5,7 @@ require_once 'db_link.php';
 $query = sprintf("SELECT * FROM favourites JOIN recipe ON id_recipe=recipe.id WHERE id_user='%s'",$_SESSION['userid']);
 $result = $connec -> executeQuery($query);
 while ($fav = mysql_fetch_assoc($result)) {
-    $ret[] = sprintf("Ext.create('thermocook.Main.View.favourite',{
+    $ret[] = sprintf("Ext.create('thermocook.Main.View.Favourite',{
     items : [{
         xtype : 'panel',
         html : '%s',
